493cfc38f8dce2d10e9c0211fe957799f69770c0
Antoine <aaltorffer@orange.fr>
2024-05-03 20:46:57 +0200

    Parse GLSL error message

--- shader-language-server/src/glsl.rs
+++ shader-language-server/src/glsl.rs
@@ -2,7 +2,6 @@ use std::path::Path;
 use crate::{shader_error::{ShaderError, ShaderErrorList, ShaderErrorSeverity}, common::{Validator, ShaderTree}};
 use glsl::parser::Parse;
 use glsl::syntax::ShaderStage;
-
 pub struct Glsl {
 }
 
@@ -16,7 +15,43 @@ impl Glsl {
 impl Glsl {
     fn parse_errors(errors: &String) -> Result<ShaderErrorList, ShaderErrorList>
     {
-        return Err(ShaderErrorList::from(ShaderError::ValidationErr { emitted: errors.to_string(), src: String::from("") }));
+        let mut shader_error_list = ShaderErrorList::empty();
+
+        let reg = regex::Regex::new(r"(?m)^(.*?: \d+: at line \d+):$")?;
+        let mut starts = Vec::new();
+        for capture in reg.captures_iter(errors.as_str()) {
+            starts.push(capture.get(0).unwrap().start());
+        }
+        starts.push(errors.len());
+        let internal_reg = regex::Regex::new(r"(?s)^(.*?): (\d+): at line (\d+):(.+)")?;
+        for start in 0..starts.len()-1 {
+            let first = starts[start];
+            let length = starts[start + 1] - starts[start];
+            let block : String = errors.chars().skip(first).take(length).collect();
+            if let Some(capture) = internal_reg.captures(block.as_str()) {
+                let level = capture.get(1).map_or("", |m| m.as_str());
+                let _error_index = capture.get(2).map_or("", |m| m.as_str());
+                let line = capture.get(3).map_or("", |m| m.as_str());
+                let msg = capture.get(4).map_or("", |m| m.as_str());
+                shader_error_list.push(ShaderError::ParserErr {
+                    severity: match level {
+                        "error" => ShaderErrorSeverity::Error,
+                        "warning" => ShaderErrorSeverity::Warning,
+                        "note" => ShaderErrorSeverity::Information,
+                        "hint" => ShaderErrorSeverity::Hint,
+                        _ => ShaderErrorSeverity::Error,
+                    },
+                    error: String::from(msg),
+                    line: line.parse::<usize>().unwrap_or(0),
+                    pos: 0, // Could we retrieve pos ?
+                });
+            }
+            else 
+            {
+                shader_error_list.push(ShaderError::InternalErr(format!("Failed to parse regex: {}", block)));
+            }
+        }
+        return Ok(shader_error_list);
     }
 }
 impl Validator for Glsl {
