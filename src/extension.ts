4b20dc45281c44d344e7c4cb7eee690d0a26f6f2
Antoine <aaltorffer@orange.fr>
2025-02-13 13:20:31 +0100

    Test vscode ui API for entry points.

--- src/extension.ts
+++ src/extension.ts
@@ -35,6 +35,141 @@ export async function activate(context: vscode.ExtensionContext)
             return; // Extension failed to launch.
         }
     }
+
+
+
+
+
+    const iconDecorationType = vscode.window.createTextEditorDecorationType({
+        // Icon
+        gutterIconPath: context.asAbsolutePath('./res/icons/hlsl-icon.svg'),
+        gutterIconSize: "contain",
+        // Minimap
+        overviewRulerColor: "rgb(255, 0, 255)",  
+        overviewRulerLane: vscode.OverviewRulerLane.Full,  
+        rangeBehavior: vscode.DecorationRangeBehavior.OpenOpen,
+        // Border
+		borderWidth: '1px',
+		borderStyle: 'solid',
+    });
+
+    let activeEditor = vscode.window.activeTextEditor;
+    function updateDecorations() {
+        if (activeEditor) {
+            // TODO: to gather this, hijack hover
+            // On hover, if function, add button to set as entry point. register in toolbar aswell.
+            // Pass to lsp
+            const decoration0 = { range: new vscode.Range(new vscode.Position(5, 0), new vscode.Position(5, 0)), hoverMessage: 'Icon decoration' };
+            const decoration1 = { range: new vscode.Range(new vscode.Position(10, 2), new vscode.Position(10, 10)), hoverMessage: 'Small decoration' };
+            const decoration2 = { range: new vscode.Range(new vscode.Position(12, 0), new vscode.Position(12, 5)), hoverMessage: 'Large decoration' };
+            activeEditor.setDecorations(iconDecorationType, [decoration0, decoration1, decoration2]);
+        }
+    }
+	if (activeEditor) {
+        // First open on IDE
+		updateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+            updateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+            updateDecorations();
+		}
+	}, null, context.subscriptions);
+
+
+
+
+
+
+    // https://code.visualstudio.com/api/ux-guidelines/sidebars
+    // https://github.com/RedCMD/TmLanguage-Syntax-Highlighter/blob/main/package.json
+    // https://code.visualstudio.com/api/extension-guides/tree-view
+    class FtpNode {
+        resource: vscode.Uri = vscode.Uri.parse("");
+        isDirectory: boolean = false;
+    }
+    class FtpTreeDataProvider implements vscode.TreeDataProvider<FtpNode>, vscode.TextDocumentContentProvider {
+
+        private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+        readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+    
+    
+        public refresh() {
+            this._onDidChangeTreeData.fire(undefined);
+        }
+    
+    
+        public getTreeItem(element: FtpNode): vscode.TreeItem {
+            return {
+                resourceUri: element.resource,
+                collapsibleState: element.isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : void 0,
+                command: element.isDirectory ? void 0 : {
+                    command: 'ftpExplorer.openFtpResource',
+                    arguments: [element.resource],
+                    title: 'Open FTP Resource'
+                }
+            };
+        }
+    
+        public getChildren(element?: FtpNode): FtpNode[] | Thenable<FtpNode[]> {
+            return element ? [element] : [];
+        }
+    
+        public getParent(element: FtpNode): FtpNode | undefined {
+            const parent = element.resource.with({ path: element.resource.path });
+            return parent.path !== '//' ? { resource: parent, isDirectory: true } : undefined;
+        }
+    
+        public provideTextDocumentContent(uri: vscode.Uri, _token: vscode.CancellationToken): vscode.ProviderResult<string> {
+            return "salut";
+        }
+    }
+    
+    class FtpExplorer {
+    
+        private ftpViewer: vscode.TreeView<FtpNode>;
+    
+        constructor(context: vscode.ExtensionContext) {
+            const treeDataProvider = new FtpTreeDataProvider();
+            context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('shader-validator', treeDataProvider));
+    
+            this.ftpViewer = vscode.window.createTreeView('shader-validator', { treeDataProvider });
+    
+            vscode.commands.registerCommand('shader-validator.refresh', () => treeDataProvider.refresh());
+        }
+    
+        private openResource(resource: vscode.Uri): void {
+            vscode.window.showTextDocument(resource);
+        }
+    
+        private async reveal(): Promise<void> {
+            const node = this.getNode();
+            if (node) {
+                return this.ftpViewer.reveal(node);
+            }
+        }
+    
+        private getNode(): FtpNode | undefined {
+            if (vscode.window.activeTextEditor) {
+                if (vscode.window.activeTextEditor.document.uri.scheme === 'ftp') {
+                    return { resource: vscode.window.activeTextEditor.document.uri, isDirectory: false };
+                }
+            }
+            return undefined;
+        }
+    }
+    let explorer = new FtpExplorer(context);
+
+
+
+    
     // Create language client
     const client = await createLanguageClient(context);
     if (client === null) {
