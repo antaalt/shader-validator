64b63f047ffc257483226b011dab52113554805e
Antoine <aaltorffer@orange.fr>
2025-02-16 00:39:55 +0100

    Code cleaning & UI improvements

--- src/shaderVariant.ts
+++ src/shaderVariant.ts
@@ -89,22 +89,28 @@ export class ShaderVariantTreeDataProvider implements vscode.TreeDataProvider<Sh
                     }
                 ]
             };
+            item.description = `[${element.defines.defines.map(d => d.label).join(",")}]`;
+            item.tooltip = `Shader variant ${element.name}`;
             item.checkboxState = vscode.TreeItemCheckboxState.Unchecked;
             item.iconPath = new vscode.ThemeIcon('code');
             item.contextValue = element.kind;
             return item;
         } else if (element.kind === 'file') {
-            let item = new vscode.TreeItem(element.uri.fsPath, vscode.TreeItemCollapsibleState.Expanded);
+            let item = new vscode.TreeItem(vscode.workspace.asRelativePath(element.uri.path), vscode.TreeItemCollapsibleState.Expanded);
+            item.description = `${element.variants.length}`;
+            item.tooltip = `File ${element.uri.fsPath}`;
             item.iconPath = vscode.ThemeIcon.File;
             item.contextValue = element.kind;
             return item;
         } else if (element.kind === 'defineList') {
             let item = new vscode.TreeItem("defines", vscode.TreeItemCollapsibleState.Expanded);
+            item.description = `${element.defines.length}`;
             item.iconPath = new vscode.ThemeIcon('keyboard');
             item.contextValue = element.kind;
             return item;
         } else if (element.kind === 'includeList') {
             let item = new vscode.TreeItem("includes", vscode.TreeItemCollapsibleState.Expanded);
+            item.description = `${element.includes.length}`;
             item.tooltip = `User defined include ${element.includes}`,
             item.iconPath = new vscode.ThemeIcon('file-code');
             item.contextValue = element.kind;
@@ -133,7 +139,7 @@ export class ShaderVariantTreeDataProvider implements vscode.TreeDataProvider<Sh
     public getChildren(element?: ShaderVariantNode): ShaderVariantNode[] | Thenable<ShaderVariantNode[]> {
         if (element) {
             if (element.kind === 'variant') {
-                return [element.stage, element.includes, element.defines];
+                return [element.stage, element.defines, element.includes];
             } else if (element.kind === 'file') {
                 return element.variants;
             } else if (element.kind === 'includeList') {
@@ -155,62 +161,7 @@ export class ShaderVariantTreeDataProvider implements vscode.TreeDataProvider<Sh
         }
     }
 
-    public addShaderVariant(uri: vscode.Uri, name: string): void {
-        let cachedFile = this.files.get(uri);
-        let newShaderVariant : ShaderVariant = {
-            kind: 'variant',
-            uri: uri,
-            name: name,
-            isActive: false,
-            stage: {
-                kind: 'stage',
-                stage: ShaderStage.auto
-            },
-            defines: {
-                kind: 'defineList',
-                defines:[]
-            },
-            includes: {
-                kind: 'includeList',
-                includes:[]
-            }
-        };
-        if (cachedFile) {
-            cachedFile.variants.push(newShaderVariant);
-        } else {
-            let newFile : ShaderVariantFile = {
-                kind: 'file',
-                uri: uri,
-                variants: [newShaderVariant]
-            };
-            this.files.set(uri, newFile);
-        }
-        this.refresh();
-    }
-    public deleteShaderVariant(entryPoint: ShaderVariant): void {
-        let cachedFile = this.files.get(entryPoint.uri);
-        if (cachedFile) {
-            let index = cachedFile.variants.indexOf(entryPoint);
-            if (index > -1) {
-                delete cachedFile.variants[index];
-            }
-        }
-        this.refresh();
-    }
-
-    public clearShaderVariant(uri: vscode.Uri): void {
-        let cachedFile = this.files.get(uri);
-        if (cachedFile) {
-            if (cachedFile.kind === "file") {
-                cachedFile.variants = [];
-                this.refresh();
-            } else {
-                // unreachable
-            }
-        } else {
-            console.warn("No cached file ", uri);
-        }
-    }
+    // TODO: instead of addFile, should have all workspace file in tree ?
     public addFile(uri: vscode.Uri): void {
         let newFile : ShaderVariantFile = {
             kind: 'file',
@@ -224,12 +175,168 @@ export class ShaderVariantTreeDataProvider implements vscode.TreeDataProvider<Sh
         this.files.delete(uri);
         this.refresh();
     }
-    public visitShaderVariants(uri: vscode.Uri, callback: (e:string, active: boolean) => void) {
+    public add(node: ShaderVariantNode) {
+        if (node.kind === 'file') {
+            node.variants.push({
+                kind: 'variant',
+                uri: node.uri,
+                name: 'main',
+                isActive: false,
+                stage: {
+                    kind: 'stage',
+                    stage: ShaderStage.auto
+                },
+                defines: {
+                    kind: 'defineList',
+                    defines:[]
+                },
+                includes: {
+                    kind: 'includeList',
+                    includes:[]
+                }
+            });
+            this.refresh();
+        } else if (node.kind === 'defineList') {
+            node.defines.push({
+                kind: "define",
+                label: "MY_MACRO",
+                value: "0",
+            });
+            this.refresh();
+        } else if (node.kind === 'includeList') {
+            node.includes.push({
+                kind: "include",
+                include: "C:/",
+            });
+            this.refresh();
+        }
+    }
+    public async edit(node: ShaderVariantNode) {
+        if (node.kind === 'variant') {
+            let name = await vscode.window.showInputBox({
+                title: "Entry point selection",
+                value: node.name,
+                prompt: "Select an entry point name for your variant",
+                placeHolder: "main"
+            });
+            if (name) {
+                node.name = name;
+                this.refresh();
+            }
+        } else if (node.kind === 'define') {
+            let label = await vscode.window.showInputBox({
+                title: "Macro label",
+                value: node.label,
+                prompt: "Select a label for you macro.",
+                placeHolder: "MY_MACRO"
+            });
+            let value = await vscode.window.showInputBox({
+                title: "Macro value",
+                value: node.value,
+                prompt: "Select a value for you macro.",
+                placeHolder: "0"
+            });
+            if (label) {
+                node.label = label;
+            }
+            if (value) {
+                node.value = value;
+            }
+            if (value || label) {
+                this.refresh();
+            }
+        } else if (node.kind === 'include') {
+            let include = await vscode.window.showInputBox({
+                title: "Include path",
+                value: node.include,
+                prompt: "Select a path for your include.",
+                placeHolder: "C:/Users/"
+            });
+            if (include) {
+                node.include = include;
+                this.refresh();
+            }
+        } else if (node.kind === 'stage') {
+            let stage = await vscode.window.showQuickPick(
+                [
+                    ShaderStage[ShaderStage.auto],
+                    ShaderStage[ShaderStage.vertex],
+                    ShaderStage[ShaderStage.fragment],
+                    ShaderStage[ShaderStage.compute],
+                    ShaderStage[ShaderStage.tesselationControl],
+                    ShaderStage[ShaderStage.tesselationEvaluation],
+                    ShaderStage[ShaderStage.mesh],
+                    ShaderStage[ShaderStage.task],
+                    ShaderStage[ShaderStage.geometry],
+                    ShaderStage[ShaderStage.rayGeneration],
+                    ShaderStage[ShaderStage.closestHit],
+                    ShaderStage[ShaderStage.anyHit],
+                    ShaderStage[ShaderStage.callable],
+                    ShaderStage[ShaderStage.miss],
+                    ShaderStage[ShaderStage.intersect],
+                ],
+                {
+                    title: "Shader stage"
+                }
+            );
+            if (stage) {
+                node.stage = ShaderStage[stage as keyof typeof ShaderStage];
+                this.refresh();
+            }
+        }
+    }
+    public delete(node: ShaderVariantNode) {
+        if (node.kind === 'variant') {
+            let cachedFile = this.files.get(node.uri);
+            if (cachedFile) {
+                let index = cachedFile.variants.indexOf(node);
+                if (index > -1) {
+                    delete cachedFile.variants[index];
+                    this.refresh();
+                }
+            }
+        } else if (node.kind === 'define') {
+            // Dirty remove, might be costly when lot of elements...
+            for (let [uri, file] of this.files) {
+                let found = false;
+                for (let variant of file.variants) {
+                    let index = variant.defines.defines.indexOf(node);
+                    if (index > -1) {
+                        delete variant.defines.defines[index];
+                        this.refresh();
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+        } else if (node.kind === 'include') {
+            // Dirty remove, might be costly when lot of elements...
+            for (let [uri, file] of this.files) {
+                let found = false;
+                for (let variant of file.variants) {
+                    let index = variant.includes.includes.indexOf(node);
+                    if (index > -1) {
+                        delete variant.includes.includes[index];
+                        this.refresh();
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) {
+                    break;
+                }
+            }
+        }
+    }
+    public visitShaderVariants(uri: vscode.Uri, callback: (variant: ShaderVariant) => void) {
         let cachedFile = this.files.get(uri);
         if (cachedFile) {
             if (cachedFile.kind === 'file') {
-                for (let file of cachedFile.variants) {
-                    callback(file.name, file.isActive);
+                for (let variant of cachedFile.variants) {
+                    callback(variant);
                 }
             }
         }
--- src/sidebar.ts
+++ src/sidebar.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { ShaderStage, ShaderVariantNode, ShaderVariantTreeDataProvider } from './shaderVariant';
+import { ShaderStage, ShaderVariant, ShaderVariantNode, ShaderVariantTreeDataProvider } from './shaderVariant';
 
 export class Sidebar {
     private provider : ShaderVariantTreeDataProvider;
@@ -26,108 +26,13 @@ export class Sidebar {
         context.subscriptions.push(vscode.window.registerTreeDataProvider('shader-validator-variants', this.provider));
 
         context.subscriptions.push(vscode.commands.registerCommand("shader-validator.addMenu", (node: ShaderVariantNode): void => {
-            if (node.kind === 'file') {
-                this.provider.addShaderVariant(node.uri, "main");
-                this.provider.refresh();
-            } else if (node.kind === 'defineList') {
-                node.defines.push({
-                    kind: "define",
-                    label: "MY_MACRO",
-                    value: "0",
-                });
-                this.provider.refresh();
-            } else if (node.kind === 'includeList') {
-                node.includes.push({
-                    kind: "include",
-                    include: "C:/",
-                });
-                this.provider.refresh();
-            }
+            this.provider.add(node);
         }));
         context.subscriptions.push(vscode.commands.registerCommand("shader-validator.deleteMenu", (node: ShaderVariantNode) => {
-            if (node.kind === 'variant') {
-                this.provider.deleteShaderVariant(node);
-                this.provider.refresh();
-            } else if (node.kind === 'define') {
-                //node.defines.indexOf(node);
-                //this.provider.refresh();
-            }
+            this.provider.delete(node);
         }));
         context.subscriptions.push(vscode.commands.registerCommand("shader-validator.editMenu", async (node: ShaderVariantNode) => {
-            
-            if (node.kind === 'variant') {
-                let name = await vscode.window.showInputBox({
-                    title: "Entry point selection",
-                    value: node.name,
-                    prompt: "Select an entry point name for your variant",
-                    placeHolder: "main"
-                });
-                if (name) {
-                    node.name = name;
-                    this.provider.refresh();
-                }
-            } else if (node.kind === 'define') {
-                let label = await vscode.window.showInputBox({
-                    title: "Macro label",
-                    value: node.label,
-                    prompt: "Select a label for you macro.",
-                    placeHolder: "MY_MACRO"
-                });
-                let value = await vscode.window.showInputBox({
-                    title: "Macro value",
-                    value: node.value,
-                    prompt: "Select a value for you macro.",
-                    placeHolder: "0"
-                });
-                if (label) {
-                    node.label = label;
-                }
-                if (value) {
-                    node.value = value;
-                }
-                if (value || label) {
-                    this.provider.refresh();
-                }
-            } else if (node.kind === 'include') {
-                let include = await vscode.window.showInputBox({
-                    title: "Include path",
-                    value: node.include,
-                    prompt: "Select a path for your include.",
-                    placeHolder: "C:/Users/"
-                });
-                if (include) {
-                    node.include = include;
-                    this.provider.refresh();
-                }
-            } else if (node.kind === 'stage') {
-
-                let stage = await vscode.window.showQuickPick(
-                    [
-                        ShaderStage[ShaderStage.auto],
-                        ShaderStage[ShaderStage.vertex],
-                        ShaderStage[ShaderStage.fragment],
-                        ShaderStage[ShaderStage.compute],
-                        ShaderStage[ShaderStage.tesselationControl],
-                        ShaderStage[ShaderStage.tesselationEvaluation],
-                        ShaderStage[ShaderStage.mesh],
-                        ShaderStage[ShaderStage.task],
-                        ShaderStage[ShaderStage.geometry],
-                        ShaderStage[ShaderStage.rayGeneration],
-                        ShaderStage[ShaderStage.closestHit],
-                        ShaderStage[ShaderStage.anyHit],
-                        ShaderStage[ShaderStage.callable],
-                        ShaderStage[ShaderStage.miss],
-                        ShaderStage[ShaderStage.intersect],
-                    ],
-                    {
-                        title: "Shader stage"
-                    }
-                );
-                if (stage) {
-                    node.stage = ShaderStage[stage as keyof typeof ShaderStage];
-                    this.provider.refresh();
-                }
-            }
+            this.provider.edit(node);
         }));
     }
     async didOpenDocument(document: vscode.TextDocument, next: (data: vscode.TextDocument) => Promise<void>) : Promise<void> {
@@ -142,10 +47,10 @@ export class Sidebar {
     private updateDecorations() {
         if (this.activeEditor) {
             let decorations : vscode.DecorationOptions[]= [];
-            this.provider.visitShaderVariants(this.activeEditor.document.uri, (message: string, active: boolean) => {
-                if (active) {
-                    // TODO: get range
-                    decorations.push({ range: new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)), hoverMessage: message });
+            this.provider.visitShaderVariants(this.activeEditor.document.uri, (variant: ShaderVariant) => {
+                if (variant.isActive) {
+                    // TODO: get range of variant.name from server.
+                    decorations.push({ range: new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0)), hoverMessage: variant.name });
                 }
             });
             this.activeEditor.setDecorations(this.decorator, decorations);
