dcb26323ef53ed6243d481de96537a239046221b
Antoine <aaltorffer@orange.fr>
2026-08-02 16:27:42 +0200

    Restore variant test as it was also a document cache issue.

--- src/test/suite/variant.test.ts
+++ src/test/suite/variant.test.ts
@@ -1,17 +1,18 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import { activate } from './utils';
+import { activate, openAndShowFile, testDiagnostic, testHasDocumentSymbol } from './utils';
 
 suite('Server variant Test Suite', () => {
-    /*test('Check variant config', async () => {
+    test('Check variant config', async () => {
         const docUri = await vscode.workspace.findFiles("test.variant.hlsl");
         assert.ok(docUri.length > 0);
-        let data = await activate(docUri[0], true)!;
+        await activate(true)!;
         // Load database
         const configUri = await vscode.workspace.findFiles("variant.config.json");
         await vscode.commands.executeCommand("shader-validator.loadVariantDatabaseFromUri", configUri[0]);
-        // Check no diagnostics
-        let diagnostics = vscode.languages.getDiagnostics(docUri[0]);
-        assert.ok(diagnostics.length === 0, "Diagnostic is not empty: " + JSON.stringify(diagnostics));
-    }).timeout(10000); // First test to run that start the server. Increase timeout to be sure its not timed out.*/
+        // Validate file
+        await openAndShowFile(docUri[0]);
+        await testDiagnostic(docUri[0], false);
+        await testHasDocumentSymbol(docUri[0], "mainOk");
+    }).timeout(10000); // First test to run that start the server. Increase timeout to be sure its not timed out.
 });
