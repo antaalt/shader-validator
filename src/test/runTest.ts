6d1d8c10e6af4b6f42c9e2c46a71ffba9e3dfb9c
Antoine <aaltorffer@orange.fr>
2026-08-29 21:45:39 +0200

    Add browser test

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,18 +1,11 @@
 import * as path from 'path';
 
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from '@vscode/test-electron';
+import { runTests as runTestsWeb } from '@vscode/test-web';
 import * as cp from 'child_process';
 
-async function main() {
+async function native(extensionDevelopmentPath: string, extensionTestsPath: string) {
 	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-		
 		// Spawn vscode ourselve to install extension we are relying on.
 		const vscodeExecutablePath = await downloadAndUnzipVSCode();
 		const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
@@ -44,6 +37,38 @@ async function main() {
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
+	}
+}
+
+async function web(extensionDevelopmentPath: string, extensionTestsPath: string) {
+	try {
+		// Run integration test in a web version of vscode.
+		await runTestsWeb({
+			browserType: 'chromium',
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			folderPath: path.resolve(__dirname, '../../test/'),
+			extensionIds: [{ id: "ms-vscode.wasm-wasi-core" }]
+		});
+	} catch (err) {
+		console.error('Failed to run tests');
+		process.exit(1);
+	}
+}
+
+async function main() {
+	// The folder containing the Extension Manifest package.json
+	// Passed to `--extensionDevelopmentPath`
+	const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+	// The path to test runner
+	// Passed to --extensionTestsPath
+	const extensionTestsPath = path.resolve(__dirname, './suite/index');
+	
+	if (process.env.TEST_IN_BROWSER === "true") {
+		return web(extensionDevelopmentPath, extensionTestsPath);
+	} else {
+		return native(extensionDevelopmentPath, extensionTestsPath);
 	}
 }
 
